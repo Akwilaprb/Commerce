c9e12932009d60d6ab10ceeb3813a70c5c26f0c1
akwila <akwilanadindapurba@gmail.com>
2023-12-06 21:21:43 +0700

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "WMP_Group_Project"
+include(":app")
+ 
